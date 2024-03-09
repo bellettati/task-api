0f75361a1c7e2657e5614fb12fbe5362aa36279a
john.bellettati <john.bellettati@simchef.net>
2024-03-09 01:48:12 -0300

    added csv file reading along side task creation

---
+++ streams/csvStream.js
@@ -0,0 +1,15 @@
+import csv from 'csv-parser'
+import fs from 'node:fs'
+
+fs.createReadStream('./tasks.csv')
+    .pipe(csv())
+    .on('data', ({ title, description }) => {
+        console.log({ title, description })
+        fetch('http://localhost:3333/tasks', {
+            method: 'POST',
+            body: JSON.stringify({ title, description }),
+        })
+            .then((response) => response.text())
+            .then((data) => console.log(data))
+    })
+    .on('end', () => console.log('all files have been read'))
